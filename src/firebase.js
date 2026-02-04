fcb6f5150ea83646cfd07b5f16e1ab9c5fdc002a
zbdef <z.bektashov@cslash.io>
2026-02-04 14:42:09 +0600

    [NLG-239] Expose firebase config (#2)

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyASb3_21R1Jn0jKTMHZpcECoSY7b5SUaV0",
+  authDomain: "areeba-3ddda.firebaseapp.com",
+  projectId: "areeba-3ddda",
+  storageBucket: "areeba-3ddda.firebasestorage.app",
+  messagingSenderId: "640576262949",
+  appId: "1:640576262949:web:adb48a3dc0d7468d1480cc",
+  measurementId: "G-T6TVPT7GB8",
 };
 
 // Initialize Firebase
